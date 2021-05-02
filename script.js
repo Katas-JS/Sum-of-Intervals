4c3d526d448d6decabb5b3028784e9efa4b348ae
princi8m <58610321+princi8m@users.noreply.github.com>
2021-05-03 01:56:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 function sumIntervals(intervals){
   //TODO
   let numberOfIntervals=intervals.length;
+  
+  //check highest negative number among all given values
+  let highestNegative=0;
+   for (a=0; a<numberOfIntervals; a++) {
+     if (intervals[a][0]<highestNegative){highestNegative=intervals[a][0]; console.log("h",highestNegative)}
+   } // end for a
+  //add that highest number to all values
+  if (highestNegative<0){
+    
+    highestNegative=highestNegative*-1;
+  for (a=0; a<numberOfIntervals; a++) {
+    
+    intervals[a][0]=intervals[a][0]+highestNegative;
+    intervals[a][1]=intervals[a][1]+highestNegative;
+   
+  }
+  }
+  
+  //start summing intervals
   var arr=[];
   let d=0;
   for (a=0; a<numberOfIntervals; a++) {
-   // console.log(intervals[a][0]);
-    for (b=intervals[a][0]; b<intervals[a][1]; b++){
+
+    let val1=intervals[a][0];
+    let val2=intervals[a][1];
+
+    for (b=val1; b<val2; b++){
       //console.log(b)
       arr[b]=1;
     }// end for b
   } // end for a
   for (c=0; c<arr.length; c++){ if (arr[c]) {d=d+arr[c]}; }
   return d;
+  
 }// end function sumIntervals
